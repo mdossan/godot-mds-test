f0d925e57d7a461c161ba6453adcf359d68ec536
mdossan <mdossan@protonmail.com>
2026-06-09 16:48:28 +0200

    🚧 Add function `run_tests` in `MdsTestRunner`

--- addons/mds_test/mds_test_runner.gd
+++ addons/mds_test/mds_test_runner.gd
@@ -2,6 +2,11 @@ class_name MdsTestRunner extends Node
 
 @export var tests_folder_path: String = "res://tests"
 
+func run_tests():
+	var test_paths: Array[String] = []
+	find_tests(tests_folder_path, test_paths)
+	print(test_paths)
+
 func find_tests(current_path: String, test_paths: Array[String]) -> void:
 	var filenames = DirAccess.get_files_at(current_path)
 	for filename in filenames:
