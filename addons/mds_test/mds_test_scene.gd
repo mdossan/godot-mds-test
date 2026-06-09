db6f539ef962ecb8b35b760e9b8521edf5825494
mdossan <mdossan@protonmail.com>
2026-06-10 01:04:15 +0200

    ✨ Define methods in `MdsTestScene` to succeed, fail

--- addons/mds_test/mds_test_scene.gd
+++ addons/mds_test/mds_test_scene.gd
@@ -1,6 +1,12 @@
 class_name MdsTestScene extends Node
 
-signal test_finished()
+signal test_finished(result: MdsTestEnums.Result, message: String)
+
+func succeed(message: String = "Test executed with no error"):
+	test_finished.emit(MdsTestEnums.Result.SUCCESS, message)
+
+func fail(message: String = "Test has failed"):
+	test_finished.emit(MdsTestEnums.Result.FAILURE, message)
 
 func test():
-	test_finished.emit()
+	test_finished.emit(MdsTestEnums.Result.FAILURE, "No test function declared")
