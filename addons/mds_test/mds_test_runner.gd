faf08c7251131a980626c70e614a4f25a9eaa5bd
mdossan <mdossan@protonmail.com>
2026-06-09 16:53:26 +0200

    🚧 Mount each test scene in tree inside `run_tests`

--- addons/mds_test/mds_test_runner.gd
+++ addons/mds_test/mds_test_runner.gd
@@ -8,7 +8,13 @@ func _ready() -> void:
 func run_tests():
 	var test_paths: Array[String] = []
 	find_tests(tests_folder_path, test_paths)
-	print(test_paths)
+	print("Found %d tests" % test_paths.size())
+	for test_path in test_paths:
+		print("Running test: %s" % test_path)
+		var test_resource: PackedScene = load(test_path)
+		var test_scene: MdsTestScene = test_resource.instantiate()
+		add_child(test_scene)
+	
 
 func find_tests(current_path: String, test_paths: Array[String]) -> void:
 	var filenames = DirAccess.get_files_at(current_path)
