32231ef67c4fddf68a39d03ba81a0c25e589b587
mdossan <mdossan@protonmail.com>
2026-06-11 14:28:37 +0200

    ✨ Use new assertion signal in `MdsTestRunner`

--- addons/mds_test/mds_test_runner.gd
+++ addons/mds_test/mds_test_runner.gd
@@ -4,6 +4,10 @@ signal wait_test_end
 
 @export var tests_folder_path: String = "res://tests"
 
+var _assertion_amount: int = 0
+var _assertion_ok: int = 0
+var _assertion_fail: int = 0
+
 func _ready() -> void:
 	run_tests()
 
@@ -16,12 +20,13 @@ func run_tests():
 		print("Running test: %s" % test_path)
 		var test_resource: PackedScene = load(test_path)
 		var test_scene: MdsTestScene = test_resource.instantiate()
-		test_scene.test_finished.connect(_on_test_finished.bind(test_scene))
 		test_scene.tree_exited.connect(_on_test_unmounted)
+		test_scene.assertion.connect(_on_test_assertion)
 		add_child(test_scene)
 		# We wait an initial physics_frame to be sure all area and collision are init
 		await get_tree().physics_frame
-		test_scene.test()
+		await test_scene.test()
+		test_scene.queue_free()
 		await wait_test_end
 
 func find_tests(current_path: String, test_paths: Array[String]) -> void:
@@ -40,3 +45,14 @@ func _on_test_finished(result: MdsTestEnums.Result, message: String, test_scene:
 
 func _on_test_unmounted():
 	wait_test_end.emit()
+
+func _on_test_assertion(is_ok: bool, message: String):
+	var status = "OK"
+	if not is_ok:
+		status = "ERROR"
+	print("[%s] %s" % [status, message])
+	_assertion_amount += 1
+	if is_ok:
+		_assertion_ok += 1;
+	else:
+		_assertion_fail += 1;
